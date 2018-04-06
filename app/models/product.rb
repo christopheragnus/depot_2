1cf0b54a9ce5e95ccb17a6afcf270fda3a5477cc
Christopher Lam <Agnus@Christophers-MacBook-Pro-3.local>
2018-04-06 15:37:17 +1000

    Iteration E3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,18 @@
 class Product < ApplicationRecord
     has_many :line_items
 
-    before_destroy :ensure_not_referenced_by_any_item
+    before_destroy :ensure_not_referenced_by_any_line_item
 
     private
 
         # ensure that there are no line items referencing this product
         def ensure_not_referenced_by_any_line_item 
             unless line_items.empty?
-            errors.add(:base, 'Line Items present')
-            throw :abort 
+                errors.add(:base, 'Line Items present')
+                throw :abort 
+            
+            end
         end
-    end
     
 
     validates :title, :description, :image_url, presence: true
