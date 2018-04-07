311d3299beda13782b305264dbc5b49cc53a35b7
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-04-08 00:42:46 +1000

    Iteration_G1

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,11 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
+  belongs_to :order, optional:true
+  belongs_to :product, optional:true
   belongs_to :cart
+
+  #we define the relationship from the order to the line item
+  #indicating that all line items that belong to an order are 
+  #to be destroyed whenever the order is destroyed
 
   def total_price
     product.price * quantity
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -56,9 +56,9 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
         xhr: true
     end
     
-      assert_response :success 
-      assert_select_jquery :html, '#cart' do
-        assert_select 'tr#current_item td', /Programming Ruby 1.9/ 
-      end
+    assert_response :success 
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/ 
     end
+  end
 end
