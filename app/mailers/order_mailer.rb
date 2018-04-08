8cd1eab9f37bbb334e93711579d05442787e9db7
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-04-09 00:30:42 +1000

    Iteration_H1

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,11 +6,7 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.received.subject
   #
-  def received
-    @greeting = "Hi"
 
-    mail to: "to@example.org"
-  end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -19,7 +15,6 @@ class OrderMailer < ApplicationMailer
   #
   def shipped(order)
     @order = order 
-
     mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 
