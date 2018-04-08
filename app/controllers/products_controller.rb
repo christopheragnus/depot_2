34c760747691e457568caa39eaef06abe0ab17ea
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-04-08 16:45:46 +1000

    Iteration_G2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, 
+    :destroy, :who_bought]
 
   # GET /products
   # GET /products.json
@@ -77,4 +78,16 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
+
+  
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
+  
 end
